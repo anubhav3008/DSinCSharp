612449da43df29836fc23c35f5d816fca3f7be78
Anubhav Shrivastava <anubhav.workemail@gmail.com>
2021-05-31 07:20:09 +0530

    PRG_0008, PRG_0007
    PRG_0008
    Given a string, if one or both of the first 2 chars is 'x', return the string without those 'x' chars, and otherwise return the string unchanged. This is a little harder than it looks.
    
    withoutX2("xHi") → "Hi"
    withoutX2("Hxi") → "Hi"
    withoutX2("Hi") → "Hi"
    
    PRG_0007
    
    Given a string and a non-empty word string, return a string made of each char just before and just after every appearance of the word in the string. Ignore cases where there is no char before or after the word, and a char may be included twice if it is between two words.
    
    wordEnds("abcXY123XYijk", "XY") → "c13i"
    wordEnds("XY123XY", "XY") → "13"
    wordEnds("XY1XY", "XY") → "11"

---
+++ DSWithAbhishek/WithoutX/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+
+namespace WithoutX
+{
+    /*
+     * 
+     * PRG_0008
+Given a string, if one or both of the first 2 chars is 'x', return the string without those 'x' chars, and otherwise return the string unchanged. This is a little harder than it looks.
+
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(withoutX2("xHi"));
+            Console.WriteLine(withoutX2("Hxi"));
+            Console.WriteLine(withoutX2("Hi"));
+            Console.WriteLine(withoutX2("Hellox"));
+        }
+
+        private static string withoutX2(string x)
+        {
+            if (x.Length < 2)
+            {
+                return x;
+
+            }
+            if (x[0] != 'x' && x[1] != 'x')
+            {
+                return x;
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (i == 0 || i == 1)
+                {
+                    if (x[i] == 'x')
+                    {
+                        continue;
+                    }
+                }
+                sb.Append(x[i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ DSWithAbhishek/WordEnds/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+
+namespace WordEnds
+{
+    /*
+     * PRG_0007
+
+Given a string and a non-empty word string, return a string made of each char just before and just after every appearance of the word in the string. Ignore cases where there is no char before or after the word, and a char may be included twice if it is between two words.
+
+
+wordEnds("abcXY123XYijk", "XY") → "c13i"
+wordEnds("XY123XY", "XY") → "13"
+wordEnds("XY1XY", "XY") → "11"
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(wordEnds("abcXY123XYijk", "XY"));
+            Console.WriteLine(wordEnds("XY123XY", "XY"));
+            Console.WriteLine(wordEnds("XY1XY", "XY"));
+        }
+        private static string wordEnds(string s1, string s2)
+        {
+            if(s1== null || s1.Length ==0 || s2 == null || s2.Length == 0)
+            {
+                return s1;
+            }
+
+            int start = 0;
+            StringBuilder sb = new StringBuilder();
+
+            while(start < s1.Length)
+            {
+                int foundIndex = s1.IndexOf(s2, start);
+                if(foundIndex == -1)
+                {
+                    break;
+                }
+
+                int leftIndex = foundIndex - 1;
+                int rightIndex = foundIndex + s2.Length;
+
+                if(leftIndex >= 0)
+                {
+                    sb.Append(s1[leftIndex]);
+                }
+
+                if(rightIndex < s1.Length)
+                {
+                    sb.Append(s1[rightIndex]);
+                }
+
+                start = foundIndex + 1;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
